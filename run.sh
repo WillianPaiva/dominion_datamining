1c68c1cf278771b74d7ae6041a8c65526b2383df
WillianPaiva <vervalenpaiva@gmail.com>
2016-02-02 16:38:25 +0100

    fix mongodb download and run

--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+mkdir -p mongodb >/dev/null 2>&1
+mkdir -p mongoData
+mkdir -p mongoLog
+if [ ! -f mongodb/mongodb-linux-x86_64-3.2.1/bin/mongod ]; then
+    echo "downloading mongodb"
+    curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.2.1.tgz
+    tar -zxf mongodb-linux-x86_64-3.2.1.tgz -C mongodb
+  fi
+echo "starting database engine"
+mongodb/mongodb-linux-x86_64-3.2.1/bin/mongod --dbpath mongoData --quiet --fork --logpath mongoLog/mongod.log
+mkdir sampleLogs/temp >/dev/null 2>&1
 
-git submodule update --init --recursive
-
-cd database/
-
-scons all --disable-warnings-as-errors
-
-cd ../
-
-mkdir sampleLogs/temp
-
-
-mvn clean compile assembly:single
+echo "Compiling Dominion data mining"
+mvn -q clean compile assembly:single
 
 java -jar target/dominion_datamining-1.0-SNAPSHOT-jar-with-dependencies.jar
