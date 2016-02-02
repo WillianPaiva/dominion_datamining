d8fb5f6f7f5f1414246f81579cc49bd918f8e477
WillianPaiva <vervalenpaiva@gmail.com>
2016-02-02 21:22:29 +0100

    set up mongodb

--- src/main/java/universite/bordeaux/app/App.java
+++ src/main/java/universite/bordeaux/app/App.java
@@ -1,27 +1,36 @@
 package universite.bordeaux.app;
+
+import org.h2.jdbcx.JdbcDataSource;
 import java.io.File;
-import java.util.Arrays;
-
-import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-import com.mongodb.client.MongoDatabase;
-
+import java.sql.Connection;
 import reader.FileHandler;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
+        Logger.getLogger( "org.mongodb.driver" ).setLevel(Level.OFF);
         File child = new File("sampleLogs/");
         FileHandler test = new FileHandler(child);
-        test.runParser();
-        // String pass = "dominionDatabase";
-        // MongoCredential credential = MongoCredential.createCredential("dominion", "project", pass.toCharArray());
-        // MongoClient mongoClient = new MongoClient(new ServerAddress("151.80.32.75"),Arrays.asList(credential));
-        // MongoDatabase db = mongoClient.getDatabase("project");
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL("jdbc:h2:./test");
+        ds.setUser("sa");
+        ds.setPassword("sa");
+        Connection conn = ds.getConnection();
+
+        // test.runParser();
+        MongoClient mongoClient = new MongoClient();
+        // mongoClient.getDB("dominion")
+        MongoDatabase db = mongoClient.getDatabase("local");
+        for (String s : mongoClient.getDatabaseNames()) {
+            System.out.println(s);
+}
     }
 }
