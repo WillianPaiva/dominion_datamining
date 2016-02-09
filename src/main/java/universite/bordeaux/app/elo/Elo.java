8d21e418474edeeb8e3b4e5b8bec1c4ea3e75e26
WillianPaiva <vervalenpaiva@gmail.com>
2016-02-09 20:55:26 +0100

    created the elo calculator

---
+++ src/main/java/universite/bordeaux/app/elo/Elo.java
@@ -0,0 +1,29 @@
+package universite.bordeaux.app.elo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.lang.Math;
+
+public class Elo {
+
+    public  HashMap<String,Integer> Calculate(ArrayList<String> winners, HashMap<String,Integer>players){
+        double eloPool = 0;
+        HashMap<String,Integer> temp = new HashMap<String,Integer>();
+        for(Map.Entry<String,Integer> entry: players.entrySet()){
+            eloPool += Math.pow(10,entry.getValue()/400);
+        }
+        for(Map.Entry<String,Integer> entry: players.entrySet()){
+            if(winners.contains(entry.getKey())){
+                temp.put(entry.getKey(),(int)(entry.getValue()+((32*(1-(Math.pow(10,entry.getValue()/400)/eloPool)))/winners.size())));
+            }else{
+                double factor = 1+((double)winners.size()-1)/((double)players.size()-(double)winners.size());
+                temp.put(entry.getKey(),(int)(entry.getValue()+((32*(0-(Math.pow(10,entry.getValue()/400)/eloPool)))*factor)));
+            }
+
+        }
+        return temp;
+    }
+
+
+}
