b194a93064177423b654383b244a4ecd3a58df2d
WillianPaiva <vervalenpaiva@gmail.com>
2016-02-09 15:16:19 +0100

    clean up the main

--- src/main/java/universite/bordeaux/app/App.java
+++ src/main/java/universite/bordeaux/app/App.java
@@ -1,6 +1,8 @@
 package universite.bordeaux.app;
 
 import java.io.File;
+
+import universite.bordeaux.app.mapper.MongoMapper;
 import universite.bordeaux.app.reader.FileHandler;
 /**
  * Hello world!
@@ -10,15 +12,8 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-        // Logger.getLogger( "org.mongodb.driver" ).setLevel(Level.OFF);
         File child = new File("sampleLogs/");
         FileHandler test = new FileHandler(child);
-        // JdbcDataSource ds = new JdbcDataSource();
-        // ds.setURL("jdbc:h2:./h2data/test");
-        // ds.setUser("sa");
-        // ds.setPassword("sa");
-        // Connection conn = ds.getConnection();
-
         test.runParser();
     }
 }
