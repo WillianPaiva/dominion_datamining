dc19d4bc9291575e9f3f3cf3ddaed33520fd200f
WillianPaiva <vervalenpaiva@gmail.com>
2016-02-09 20:55:46 +0100

    testing elo

--- src/main/java/universite/bordeaux/app/App.java
+++ src/main/java/universite/bordeaux/app/App.java
@@ -1,7 +1,11 @@
 package universite.bordeaux.app;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import universite.bordeaux.app.elo.Elo;
 import universite.bordeaux.app.mapper.MongoMapper;
 import universite.bordeaux.app.reader.FileHandler;
 /**
@@ -14,6 +18,8 @@ public class App
     {
         File child = new File("sampleLogs/");
         FileHandler test = new FileHandler(child);
-        test.runParser();
+        MongoMapper t = new MongoMapper();
+        t.generateElo();
+        // test.runParser();
     }
 }
