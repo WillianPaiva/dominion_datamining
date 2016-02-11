0bf45e9b4645f7e60747caeb278b58b722a715fe
WillianPaiva <vervalenpaiva@gmail.com>
2016-02-11 14:00:35 +0100

    transform elo class on utility class

--- src/main/java/universite/bordeaux/app/elo/Elo.java
+++ src/main/java/universite/bordeaux/app/elo/Elo.java
@@ -5,9 +5,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.lang.Math;
 
-public class Elo {
+public final class Elo {
 
-    public  HashMap<String,Integer> Calculate(ArrayList<String> winners, HashMap<String,Integer>players){
+    private Elo(){}
+
+    public static HashMap<String,Integer> Calculate(ArrayList<String> winners, HashMap<String,Integer>players){
         double eloPool = 0;
         HashMap<String,Integer> temp = new HashMap<String,Integer>();
         for(Map.Entry<String,Integer> entry: players.entrySet()){
@@ -18,7 +20,9 @@ public class Elo {
                 temp.put(entry.getKey(),(int)(entry.getValue()+((32*(1-(Math.pow(10,entry.getValue()/400)/eloPool)))/winners.size())));
             }else{
                 double factor = 1+((double)winners.size()-1)/((double)players.size()-(double)winners.size());
-                temp.put(entry.getKey(),(int)(entry.getValue()+((32*(0-(Math.pow(10,entry.getValue()/400)/eloPool)))*factor)));
+                int elo = (int)(entry.getValue()+((32*(0-(Math.pow(10,entry.getValue()/400)/eloPool)))*factor));
+                elo = (elo >= 0)? elo : 0 ;
+                temp.put(entry.getKey(),elo);
             }
 
         }
