ca0ee23b9505e16f633b4cebbb6233130230919d
WillianPaiva <vervalenpaiva@gmail.com>
2016-02-13 00:33:03 +0100

    starting elo generation

--- src/main/java/universite/bordeaux/app/App.java
+++ src/main/java/universite/bordeaux/app/App.java
@@ -1,11 +1,10 @@
 package universite.bordeaux.app;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import universite.bordeaux.app.mapper.MongoMapper;
+import universite.bordeaux.app.elo.EloGenerator;
 import universite.bordeaux.app.reader.FileHandler;
 /**
  * Hello world!
@@ -15,8 +14,10 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
+        Logger.getLogger( "org.mongodb.driver" ).setLevel(Level.OFF);
         File child = new File("sampleLogs/");
         FileHandler test = new FileHandler(child);
-         test.runParser(10);
+         test.runParser(5);
+         EloGenerator.Generate();
     }
 }
