f0e53111fed13467635b3e5ab52edc940c79b049
WillianPaiva <vervalenpaiva@gmail.com>
2016-02-13 00:33:17 +0100

    bug fix on elo calculation

--- src/main/java/universite/bordeaux/app/elo/Elo.java
+++ src/main/java/universite/bordeaux/app/elo/Elo.java
@@ -19,8 +19,8 @@ public final class Elo {
             if(winners.contains(entry.getKey())){
                 temp.put(entry.getKey(),(int)(entry.getValue()+((32*(1-(Math.pow(10,entry.getValue()/400)/eloPool)))/winners.size())));
             }else{
-                double factor = 1+((double)winners.size()-1)/((double)players.size()-(double)winners.size());
-                int elo = (int)(entry.getValue()+((32*(0-(Math.pow(10,entry.getValue()/400)/eloPool)))*factor));
+                // double factor = 1+((double)winners.size()-1)/((double)players.size()-(double)winners.size());
+                int elo = (int)(entry.getValue()+(32*(0-(Math.pow(10,entry.getValue()/400)/eloPool))));
                 elo = (elo >= 0)? elo : 0 ;
                 temp.put(entry.getKey(),elo);
             }
