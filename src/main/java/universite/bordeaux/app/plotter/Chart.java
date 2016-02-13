fb47d5e61862dc3a8133585ede4e48026a6b2dfa
WillianPaiva <vervalenpaiva@gmail.com>
2016-02-13 19:13:53 +0100

    plating the elo curve of the top 5 players

--- src/main/java/universite/bordeaux/app/plotter/Chart.java
+++ src/main/java/universite/bordeaux/app/plotter/Chart.java
@@ -1,60 +1,75 @@
 package universite.bordeaux.app.plotter;
 
-import org.jfree.chart.ChartPanel;
-
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import org.bson.Document;
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.xy.XYSplineRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 
-import com.mongodb.client.FindIterable;
 import com.mongodb.Block;
+import com.mongodb.client.FindIterable;
 
 import universite.bordeaux.app.game.Game;
 import universite.bordeaux.app.game.player.Player;
 import universite.bordeaux.app.mapper.MongoMapper;
 
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
-
 public class Chart extends ApplicationFrame {
-    public DefaultCategoryDataset dataset ;
-
+    public XYSeries temp;
     public Chart(String applicationTitle,
                  String chartTitle,
                  String xtitle,
-                 String ytitle,
-                 String player){
+                 String ytitle){
       super(applicationTitle);
-      JFreeChart lineChart = ChartFactory.createLineChart(
+      JFreeChart lineChart = ChartFactory.createXYLineChart(
                                                           chartTitle,
                                                           xtitle,ytitle,
-                                                          EloChart(player),
+                                                          EloChart(),
                                                           PlotOrientation.VERTICAL,
                                                           true,true,false);
+      XYSplineRenderer t = new XYSplineRenderer();
+      t.setSeriesShapesVisible(0, false);
+      t.setSeriesShapesVisible(1, false);
+      t.setSeriesShapesVisible(2, false);
+      t.setSeriesShapesVisible(3, false);
+      t.setSeriesShapesVisible(4, false);
+      lineChart.getXYPlot().setRenderer(t);
       ChartPanel chartPanel = new ChartPanel( lineChart );
       chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
       setContentPane( chartPanel );
 
        }
 
-    public DefaultCategoryDataset EloChart(final String player){
-        dataset = new DefaultCategoryDataset();
-        FindIterable it = MongoMapper.getPlayerGames(player);
-        it.forEach(new Block<Document>() {
 
-                @Override
-                public void apply(final Document document) {
-                    Game g = new Game(document);
-                    Player p = g.getPlayer(player);
-                    SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
-                    String DateToStr = format.format(g.getDate());
-                    dataset.addValue(p.getGameElo(),"Elo",DateToStr);
+    public XYDataset EloChart(){
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        final ArrayList<String> players = MongoMapper.getRank(5);
 
-                }
-            });
+
+        for(final String player: players){
+            temp = new XYSeries(player);
+            FindIterable<Document> it = MongoMapper.getPlayerGames(player);
+            final int total = MongoMapper.getPlayerTotalGames(player);
+            it.forEach(new Block<Document>() {
+                    int x = 0;
+                    float test;
+                    @Override
+                    public void apply(final Document document) {
+                        test = ((float)x/(float)total)*100;
+                        final Game g = new Game(document);
+                        final Player p = g.getPlayer(player);
+                        temp.add((double) Math.round(test/5),(double)p.getGameElo());
+                        x++;
+                    }
+                });
+            dataset.addSeries(temp);
+        }
         return dataset;
 
 
