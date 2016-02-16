2493ff2ab20b0939cb0ece453ec20065e24d4476
ebayol <ebayol@lebris.emi.u-bordeaux1.fr>
2016-02-16 12:02:34 +0100

    Adding javadoc comments the Player class

--- src/main/java/universite/bordeaux/app/game/player/Player.java
+++ src/main/java/universite/bordeaux/app/game/player/Player.java
@@ -6,6 +6,10 @@ import java.util.Map.Entry;
 
 import org.bson.Document;
 
+/**
+ * @author ebayol
+ *
+ */
 public class Player {
     private String pl ;
     private HashMap<String,Integer> victoryCards = new HashMap<String,Integer>();
@@ -17,10 +21,18 @@ public class Player {
     private int GameElo  = 1000;
 
 
+    /**
+     * Creates and returns the Player object based on a string (player name)
+     * @param pl
+     */
     public Player(String pl){
         this.pl = pl ;
     }
 
+    /**
+     * Loads player data from a Document object
+     * @param doc
+     */
     public Player(Document doc){
         this.pl = doc.get("name",String.class);
         this.points = doc.get("points", Integer.class);
@@ -33,6 +45,10 @@ public class Player {
 
     }
 
+    /**
+     * Creates a Document object containing the information of the Player object
+     * @return Document object
+     */
     public Document toDoc(){
         return new Document()
             .append("name",this.pl)
@@ -45,6 +61,11 @@ public class Player {
             .append("opening",opening);
     }
 
+    /**
+     * converts Document to HashMap
+     * @param doc
+     * @return HashMap
+     */
     private HashMap<String,Integer> doctohash(Document doc){
         HashMap<String,Integer> temp = new HashMap<String,Integer>();
         for(Map.Entry<String,Object> x: doc.entrySet()){
@@ -54,6 +75,11 @@ public class Player {
 
     }
 
+    /**
+     * Converts HashMap to Document
+     * @param map
+     * @return Document
+     */
     private Document hashtodoc(HashMap<String,Integer> map){
         Document temp = new Document();
         for(String x: map.keySet()){
@@ -62,44 +88,70 @@ public class Player {
         return temp;
     }
 
+    /**
+     * Returns the player name
+     * @return String, player name
+     */
     public String getPlayerName(){
         return this.pl;
     }
 
-    public void setPlayer(){
-        this.pl = pl;
+    /**
+     * @param quantity Card quantity
+     * @param cardName Card name
+     */
+    public void insertVictoryCard(int quantity , String cardName){
+        victoryCards.put(cardName, quantity);
     }
-
-    public void insertVictoryCard(int n , String s){
-        victoryCards.put(s,n);
-    }
+    /**
+     * return the cards used to count the victoy points such as Estate, Duchy, Province, Colony
+     * @return HashMap with the cards and their amount
+     */
     public HashMap<String,Integer> getVictoryCards(){
         return this.victoryCards;
     }
 
-    public void insertDeck(int n , String s){
-        deck.put(s,n);
+    /**
+     * adds a card to the players deck
+     * @param quantity number of cards of the same type
+     * @param cardName name of the card
+     */
+    public void insertDeck(int quantity , String cardName){
+        deck.put(cardName , quantity);
     }
+    /**
+     * @return The list of cards and their amount in a players deck (cards owned)
+     */
     public HashMap<String,Integer> getDeck(){
         return this.deck;
     }
 
-
-    public void setPoints(int n){
-        this.points = n;
+    /**
+     * Sets a score for the player
+     * @param points amount of points
+     */
+    public void setPoints(int points){
+        this.points = points;
     }
+    /**
+     * @return A value containing the player's points
+     */
     public int getPoints(){
         return this.points;
     }
 
-    public void setTurns(int t){
-        this.turns = t;
+    /**
+     * @param turns the number of urns played by a player
+     */
+    public void setTurns(int turns){
+        this.turns = turns;
     }
 
-	/**
-	 * @return the gameElo
-	 */
-  public int getGameElo() {
+
+/**
+ * @return players elo
+ */
+    public int getGameElo() {
 
       return GameElo;
 	}
@@ -111,17 +163,31 @@ public class Player {
 		GameElo = gameElo;
 	}
 
+	/**
+	 * @return the turns played by a player
+	 */
 	public int getTurns(){
         return this.turns;
     }
 
+    /**
+     * @param s opening cards
+     */
     public void insertOpening(String s){
         this.opening.add(s);
     }
 
-    public void insertFirstHand(int n , String s){
-        this.firstHand.put(s,n);
+    /**
+     * puts a card and it's amount in the first hand of the player
+     * @param quantity number of the specified cards
+     * @param cardName a string with the name of the card
+     */
+    public void insertFirstHand(int quantity , String cardName){
+        this.firstHand.put(cardName, quantity);
     }
+    /**
+     * @return a HashMap containing the first hand of the player (cards and amount)
+     */
     public HashMap<String,Integer> getFirstHand(){
         return this.firstHand;
     }
