c7d65c62b705ff498c9894beaddb449384ef3eca
WillianPaiva <vervalenpaiva@gmail.com>
2016-02-16 12:02:16 +0100

    elo javadoc

--- src/main/java/universite/bordeaux/app/elo/Elo.java
+++ src/main/java/universite/bordeaux/app/elo/Elo.java
@@ -9,17 +9,28 @@ public final class Elo {
 
     private Elo(){}
 
+	/**
+   * calculates the elo of a match
+	 *
+   * @param winners list of the winners on the match
+   * @param players map of the players on the match with the actual elo
+   * @return a map of players with the new elo
+	 */
     public static HashMap<String,Integer> Calculate(ArrayList<String> winners, HashMap<String,Integer>players){
         double eloPool = 0;
+
+        //calculates the total elo on the match (the eloPool)
         HashMap<String,Integer> temp = new HashMap<String,Integer>();
         for(Map.Entry<String,Integer> entry: players.entrySet()){
             eloPool += Math.pow(10,entry.getValue()/400);
         }
+
+
+        //calculates each players elo
         for(Map.Entry<String,Integer> entry: players.entrySet()){
             if(winners.contains(entry.getKey())){
                 temp.put(entry.getKey(),(int)(entry.getValue()+((32*(1-(Math.pow(10,entry.getValue()/400)/eloPool)))/winners.size())));
             }else{
-                // double factor = 1+((double)winners.size()-1)/((double)players.size()-(double)winners.size());
                 int elo = (int)(entry.getValue()+(32*(0-(Math.pow(10,entry.getValue()/400)/eloPool))));
                 elo = (elo >= 0)? elo : 0 ;
                 temp.put(entry.getKey(),elo);
