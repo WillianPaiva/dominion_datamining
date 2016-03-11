aa52f5eced5780ef1730a4ceb532e4c3124642a2
WillianPaiva <vervalenpaiva@gmail.com>
2016-03-11 16:42:37 +0100

    java doc and style review of colorstemplate.java

--- src/main/java/universite/bordeaux/app/colors/ColorsTemplate.java
+++ src/main/java/universite/bordeaux/app/colors/ColorsTemplate.java
@@ -1,24 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package universite.bordeaux.app.colors;
 
 /**
- *
+ * simple utility class to define some color for the text output.
  * @author ebayol
  */
 public final class ColorsTemplate {
+    /**
+     * setting the constructor in private.
+     */
+    private ColorsTemplate() { }
+    /**
+     * Reset the color output.
+     */
     public static final String ANSI_RESET = "\u001B[0m";
+    /**
+     * set the color output to black.
+     */
     public static final String ANSI_BLACK = "\u001B[30m";
+    /**
+     * set the color output to red.
+     */
     public static final String ANSI_RED = "\u001B[31m";
+    /**
+     * set the color output to green.
+     */
     public static final String ANSI_GREEN = "\u001B[32m";
+    /**
+     * set the color output to yellow.
+     */
     public static final String ANSI_YELLOW = "\u001B[33m";
+    /**
+     * set the color output to blue.
+     */
     public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
+    /**
+     * set the color output to purple.
+     */
+    public static final String ANSI_PURPLE = "\u001B[3k5m";
+    /**
+     * set the color output to cyan.
+     */
     public static final String ANSI_CYAN = "\u001B[36m";
+    /**
+     * set the color output to white.
+     */
     public static final String ANSI_WHITE = "\u001B[37m";
-    
 }
