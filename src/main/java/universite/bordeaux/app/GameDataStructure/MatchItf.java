de918732caa330915467b1bf4853667f7de4e988
WillianPaiva <vervalenpaiva@gmail.com>
2016-03-11 21:42:02 +0100

    refactor of the match inteface deleted some unused function

--- src/main/java/universite/bordeaux/app/GameDataStructure/MatchItf.java
+++ src/main/java/universite/bordeaux/app/GameDataStructure/MatchItf.java
@@ -1,28 +1,24 @@
 package universite.bordeaux.app.GameDataStructure;
 
 import org.bson.Document;
-import org.bson.types.ObjectId;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-
+/**
+ * Interface to access to the Match.
+ * @author Willian Ver Valen Paiva
+ */
 public interface MatchItf {
-    // Converts the Game object in the Document format, to use with mongodb server
-    public Document toDoc();
-    // Saves the Game in the database
-    public void save();
-    // Generates elo for each player of the game and saves it to the database
-    public void GenerateElo();
-    // list of the getters on the object
-    public ArrayList<String> getCardsGone();
-    public Date getDate();
-    public ArrayList<String> getMarket();
-    public PlayerItf getPlayer(String playerName);
-    public ArrayList<PlayerItf> getPlayers();
-    public ObjectId getId();
-    public ArrayList<String> getWinners();
-    public HashMap<String,Integer> getTrash();
+
+    /**
+     * Converts the Match object in the Document format.
+     * @return A Document.
+     */
+    Document toDoc();
+
+    /**
+     * save the Match in to the database.
+     */
+    void save();
+
     @Override
-    public String toString();
+    String toString();
 }
