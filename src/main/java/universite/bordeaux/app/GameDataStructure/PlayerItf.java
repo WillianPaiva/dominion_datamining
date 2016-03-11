fb88c981d54a9337ce17a953e9eabc668698b851
WillianPaiva <vervalenpaiva@gmail.com>
2016-03-11 21:57:56 +0100

    refactor of player interface deleted function that is not used and wont be used

--- src/main/java/universite/bordeaux/app/GameDataStructure/PlayerItf.java
+++ src/main/java/universite/bordeaux/app/GameDataStructure/PlayerItf.java
@@ -2,24 +2,64 @@ package universite.bordeaux.app.GameDataStructure;
 
 import org.bson.Document;
 
-import java.util.HashMap;
 
-public interface PlayerItf{
-    public Document toDoc();
-    public String getPlayerName();
-    public void insertVictoryCard(int quantity , String cardName);
-    public HashMap<String,Integer> getVictoryCards();
-    public void insertDeck(int quantity , String cardName);
-    public HashMap<String,Integer> getDeck();
-    public void setPoints(int points);
-    public int getPoints();
-    public void setTurns(int turns);
-    public int getGameElo();
-    public void setGameElo(int gameElo);
-    public int getTurns();
-    public void insertOpening(String s);
-    public void insertFirstHand(int quantity , String cardName);
-    public HashMap<String,Integer> getFirstHand();
+/**
+ * interface to provide access to the player class.
+ * @author Willian Ver Valen Paiva
+ */
+public interface PlayerItf {
+    /**
+     * creates the document version of a player object.
+     * @return a Document
+     */
+    Document toDoc();
+
+    /**
+     * return the name of the player.
+     * @return String name
+     */
+    String getPlayerName();
+
+    /**
+     * insert a victory card on list.
+     * @param quantity number of cards
+     * @param cardName card name.
+     */
+    void insertVictoryCard(final int quantity, final String cardName);
+
+    /**
+     * insert a card on players deck.
+     * @param quantity number of cards
+     * @param cardName card name
+     */
+    void insertDeck(final int quantity, final String cardName);
+
+    /**
+     * set player points.
+     * @param points number of points.
+     */
+    void setPoints(final int points);
+
+    /**
+     * set the total of turns.
+     * @param turns number of turns.
+     */
+    void setTurns(final int turns);
+
+    /**
+     * insert the opening of the player.
+     * @param s card name.
+     */
+    void insertOpening(final String s);
+
+
+    /**
+     * insert players first hand.
+     * @param quantity number of cards
+     * @param cardName card name.
+     */
+    void insertFirstHand(final int quantity, final String cardName);
+
     @Override
-    public String toString();
+    String toString();
 }
