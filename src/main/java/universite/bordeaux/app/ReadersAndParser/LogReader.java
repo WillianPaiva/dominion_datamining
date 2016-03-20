df1f8cbbe27da3c12962af688819c69698caf9d9
Khaoula Tagnaouti <ktagnaouti@emi.u-bordeaux1.fr>
2016-03-20 15:51:36 +0100

    LogReader

---
+++ src/main/java/universite/bordeaux/app/ReadersAndParser/LogReader.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package universite.bordeaux.app.ReadersAndParser;
+
+import javax.print.Doc;
+
+/**
+ *
+ * @author ktagnaouti
+ */
+public interface LogReader {
+    /**
+     * 
+     * @return object Document having information logs
+     */
+   public Doc getDoc();
+   
+}
