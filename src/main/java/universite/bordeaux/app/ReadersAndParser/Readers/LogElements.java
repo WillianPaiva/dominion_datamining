7c5bd03fa26bf6ef6448d02f2b7dd75e0f8cdf4a
WillianPaiva <vervalenpaiva@gmail.com>
2016-03-22 10:10:35 +0100

    refactoring, and fix of checkstyle mistakes

--- src/main/java/universite/bordeaux/app/ReadersAndParser/Readers/LogElements.java
+++ src/main/java/universite/bordeaux/app/ReadersAndParser/Readers/LogElements.java
@@ -1,155 +1,82 @@
 package universite.bordeaux.app.ReadersAndParser.Readers;
 
 public final class LogElements {
-	
-	public static boolean existPlaysMove(String line){
-		boolean exist = false;
-		if (line.matches("(.*) plays (a|the|an|[0-9]+)"
-                        + " \\<span class(.*)")) {
-			exist = true;
-		}
-		return exist;		
-	}
-	
-	public static boolean existBuysMove(String line){
-		boolean exist = false;
-		if(line.matches("(.*) buys (a|an|[0-9]+)"
-                + " \\<span class(.*)")){
-			exist = true;
-		}
-		return exist;
-	}
-	
-	public static boolean existDrawsOnFinishTurn(String line){
-		boolean exist = false;
-		if(line.trim()
-                .matches("\\<span class\\=logonly\\>\\((.*)"
-                        + " draws: (.*)")){
-			exist = true;
-		}
-		
-		return exist;
-	}
-	
-	public static boolean existMoveDrawDiscardCard(String line){
-		boolean exist = false;
-		if(line.trim()
-                .matches("(.*) draws and discard "
-                        + "(a|an|[0-9]+) \\<span class(.*)")){
-			exist = true;
-		}
-		return exist;
-	}
 
-	public static boolean existGainsAction(String line){
-		boolean exist = false;
-		if(line.matches("(.*) gains (a|an|[0-9]+)"
-                + " \\<span class(.*)")){
-			exist = true;
-		}
-		return exist;
-	}
-	
-	public static boolean existActionDiscardsAndGains(String line){
-		boolean exist = false;
-		if(line.matches("(.*) discards (a|an|[0-9]+)"
-                + " \\<span class(.*)")){
-			exist = true;
-			
-		}
-		return exist;
-	}
-	
-	public static boolean existTrashes(String line){
-		boolean exist = false;
-		if(line.matches("(.*) trashes (a|an|[0-9]+)"
-                + " \\<span class(.*)")){
-			exist = true;
-			
-		}
-		return exist;
-	}
-	
-	public static boolean existReveals(String line){
-		boolean exist = false;
-		if(line.matches("(.*) reveals (a|an|[0-9]+)"
-                + " \\<span class(.*)")){
-			exist = true;
-			
-		}
-		return exist;
-	}
-	
-	public static boolean existTrashingAction(String line){
-		boolean exist = false;
-		if(line.trim()
-                .matches("(.*) trashing (a|an|the|[0-9]+)"
-                        + " \\<span class(.*)")){
-			exist = true;
-		}
-		return exist;
-	}
-	
-	public static boolean existRevealingAction(String line){
-		boolean exist = false;
-		if(line.trim()
-                .matches("(.*) revealing (a|an|[0-9]+)"
-                        + " \\<span class(.*)")){
-			exist = true;
-			
-		}
-		return exist;
-	}
-	
-	public static boolean existPuttingAction(String line){
-		boolean exist = false;
-		if(line.trim().matches("(.*) putting (a|the|an|[0-9]+)"
-                + " \\<span class(.*)")){
-			exist = true;
-		}
-		return exist;
-	}
-	
-	public static boolean existGainingAction(String line){
-		boolean exist = false;
-		if(line.trim()
-                .matches("(.*) gaining "
-                        + "(a|the|another|an|[0-9]+)"
-                        + " \\<span class(.*)")){
-			exist = true;
-		}
-		return exist;
-	}
-	
-	public static boolean existDiscardingAction(String line){
-		boolean exist = false;
-		if(line.trim()
-                .matches("(.*) discarding"
-                        + " (a|an|[0-9]+)+"
-                        + " \\<span class(.*)")){
-			exist = true;
-		}
-		return exist;
-	}
-	
-	public static boolean existPlayingAction(String line){
-		boolean exist = false;
-		if(line.trim()
-                .matches("(.*) playing "
-                        + "(a|an|[0-9]+)+ "
-                        + "\\<span class(.*)")){
-			exist = true;
-		}
-		return exist;
-	}
-	
-	public static boolean existDrawingAction(String line){
-		boolean exist = false;
-		if(line.trim()
-                .matches("(.*) drawing (a|an|[0-9]+)+ "
-                        + " \\<span class(.*)")){
-			exist = true;
-		}
-		return exist;
-	}
+    public static boolean isPlaysMove(final String line) {
+        return line.matches("(.*) plays (a|the|an|[0-9]+)"
+                            + " \\<span class(.*)");
+    }
+
+    public static boolean isBuysMove(final String line) {
+        return line.matches("(.*) buys (a|an|[0-9]+)"
+                            + " \\<span class(.*)");
+    }
+
+    public static boolean isDrawsLastAction(final String line) {
+        return line.trim().matches("\\<span class\\=logonly\\>\\((.*)"
+                                   + " draws: (.*)");
+    }
+
+    public static boolean isMoveDrawDiscardCard(final String line) {
+        return line.trim().matches("(.*) draws and discard "
+                                   + "(a|an|[0-9]+) \\<span class(.*)");
+    }
+
+    public static boolean isGainsAction(final String line) {
+        return line.matches("(.*) gains (a|an|[0-9]+)"
+                            + " \\<span class(.*)");
+    }
+
+    public static boolean isActionDiscardsAndGains(final String line) {
+        return line.matches("(.*) discards (a|an|[0-9]+)"
+                            + " \\<span class(.*)");
+    }
+
+    public static boolean existTrashes(final String line) {
+        return line.matches("(.*) trashes (a|an|[0-9]+)"
+                            + " \\<span class(.*)");
+    }
+
+    public static boolean isReveals(final String line) {
+        return line.matches("(.*) reveals (a|an|[0-9]+)"
+                            + " \\<span class(.*)");
+    }
+
+    public static boolean isTrashingAction(final String line) {
+        return line.trim().matches("(.*) trashing (a|an|the|[0-9]+)"
+                                   + " \\<span class(.*)");
+    }
+
+    public static boolean isRevealingAction(final String line) {
+        return line.trim().matches("(.*) revealing (a|an|[0-9]+)"
+                                   + " \\<span class(.*)");
+    }
+
+    public static boolean isPuttingAction(final String line) {
+        return line.trim().matches("(.*) putting (a|the|an|[0-9]+)"
+                                   + " \\<span class(.*)");
+    }
+
+    public static boolean isGainingAction(final String line) {
+        return line.trim().matches("(.*) gaining "
+                                   + "(a|the|another|an|[0-9]+)"
+                                   + " \\<span class(.*)");
+    }
+
+    public static boolean isDiscardingAction(final String line) {
+        return line.trim().matches("(.*) discarding"
+                                   + " (a|an|[0-9]+)+"
+                                   + " \\<span class(.*)");
+    }
+
+    public static boolean isPlayingAction(final String line) {
+        return line.trim().matches("(.*) playing "
+                                   + "(a|an|[0-9]+)+ "
+                                   + "\\<span class(.*)");
+    }
+
+    public static boolean isDrawingAction(final String line) {
+        return line.trim().matches("(.*) drawing (a|an|[0-9]+)+ "
+                                  + " \\<span class(.*)");
+    }
 }
