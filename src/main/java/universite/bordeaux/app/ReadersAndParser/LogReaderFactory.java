3d07167b9a1cd6beddfd47d30bd192b44e9adb07
WillianPaiva <vervalenpaiva@gmail.com>
2016-03-23 22:42:04 +0100

    factory implementation

--- src/main/java/universite/bordeaux/app/ReadersAndParser/LogReaderFactory.java
+++ src/main/java/universite/bordeaux/app/ReadersAndParser/LogReaderFactory.java
@@ -2,23 +2,31 @@ package universite.bordeaux.app.ReadersAndParser;
 
 import java.io.File;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import universite.bordeaux.app.ReadersAndParser.Readers.LogReader;
+import universite.bordeaux.app.ReadersAndParser.Readers.LogReaderV1;
+import universite.bordeaux.app.ReadersAndParser.Readers.LogReaderV2;
 
 
 public final class LogReaderFactory{
-	
-	private LogReaderFactory(){
-		
-	}
 
-	public static LogReader createrReader(String logName){
-		
-		File log = new File(logName); 
-		FileReader fr = new FileReader(log);
-		
-		return  null;
-		
-	}
+  private LogReaderFactory(){
+
+  }
+
+  public static LogReader createrReader(File log){
+    FileReader fr = new FileReader(log);
+    fr.searchLineWithString("(.*)----------------------(.*)");
+    fr.jumpline();
+    Document doc = Jsoup.parse(fr.jumpline());
+    if (doc.select("b").text().contains("points")) {
+        return new LogReaderV2(log);
+    } else {
+        return new LogReaderV1(log);
+    }
+  }
 
 
 }
