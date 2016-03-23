4897f8729e26145df856705286426752843bfead
WillianPaiva <vervalenpaiva@gmail.com>
2016-03-23 20:48:43 +0100

    fix the interface Doc is not used on the project

--- src/main/java/universite/bordeaux/app/ReadersAndParser/Readers/LogReader.java
+++ src/main/java/universite/bordeaux/app/ReadersAndParser/Readers/LogReader.java
@@ -1,16 +1,12 @@
 package universite.bordeaux.app.ReadersAndParser.Readers;
 
-import javax.print.Doc;
+import org.bson.Document;
 
-/**
- *
- * @author ktagnaouti
- */
+
 public interface LogReader {
     /**
-     * 
+     *
      * @return object Document having information logs
      */
-   public Doc getDoc();
-   
+   public Document getDoc();
 }
