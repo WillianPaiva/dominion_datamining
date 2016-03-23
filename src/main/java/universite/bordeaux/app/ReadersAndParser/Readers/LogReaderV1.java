eac8cb68ebb3540bd7d87b38de9db594d99e6e6a
WillianPaiva <vervalenpaiva@gmail.com>
2016-03-23 22:45:54 +0100

    log v1 implemnentation

--- src/main/java/universite/bordeaux/app/ReadersAndParser/Readers/LogReaderV1.java
+++ src/main/java/universite/bordeaux/app/ReadersAndParser/Readers/LogReaderV1.java
@@ -1,5 +1,7 @@
 package universite.bordeaux.app.ReadersAndParser.Readers;
 
+import java.io.File;
+
 import org.bson.Document;
 
 public class LogReaderV1 extends LogReaderAbs implements LogReader{
@@ -7,15 +9,12 @@ public class LogReaderV1 extends LogReaderAbs implements LogReader{
   /**
 	 * @param fileLog name of the log file
 	 */
-	public LogReaderV1(String fileLog) {
+  public LogReaderV1(File fileLog) {
     super(fileLog);
     this.version = 1;
 	}
 
-	@Override
-	public Document getDoc() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+    public Document getDoc(){
+        return this.toDoc().append("version", this.version);
+    }
 }
