3af7b8fcbfc5750cb070fd9546f4e828294d2e30
Martha Lopez Farfan <mlfarfan@emi.u-bordeaux1.fr>
2016-03-24 22:08:20 +0100

    constansts doubles

--- src/main/java/universite/bordeaux/app/Constant/ConstantLog.java
+++ src/main/java/universite/bordeaux/app/Constant/ConstantLog.java
@@ -15,18 +15,18 @@ public final class ConstantLog {
 	public static final String LOG = "log";
 
 	public static final String PLAYS = "plays"; 
-	public static final String PLAYING = "playing"; //
+	//public static final String PLAYING = "playing"; 
  	public static final String BUYS = "buys";
 	public static final String DISCARD = "discard";
-	public static final String DISCARDING = "discarding"; //
+	//public static final String DISCARDING = "discarding"; 
 	public static final String DRAWS = "draws";
-	public static final String DRAWING = "drawing"; //
+	//public static final String DRAWING = "drawing"; 
 	public static final String GAINS = "gains";
-	public static final String GAINING = "gaining"; //
-	public static final String GAIN = "gain"; //
+	//public static final String GAINING = "gaining"; 
+	//public static final String GAIN = "gain"; 
 	public static final String TRASHMOVE = "trash";
 	public static final String REVEAL = "reveal";
-	public static final String REAVELING = "reavealing"; //
+	//public static final String REAVELING = "reavealing"; 
 	public static final String PUTTING = "putting";
 	
 }
