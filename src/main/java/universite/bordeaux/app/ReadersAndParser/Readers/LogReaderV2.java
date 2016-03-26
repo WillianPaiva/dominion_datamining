33d3b1af40bce869491ad9fc2f56d4391fe28dff
WillianPaiva <vervalenpaiva@gmail.com>
2016-03-26 22:11:45 +0100

    doc and checkstyle of logReaderV2

--- src/main/java/universite/bordeaux/app/ReadersAndParser/Readers/LogReaderV2.java
+++ src/main/java/universite/bordeaux/app/ReadersAndParser/Readers/LogReaderV2.java
@@ -7,22 +7,31 @@ import org.bson.Document;
 import universite.bordeaux.app.GameDataStructure.Player;
 import universite.bordeaux.app.GameDataStructure.PlayerItf;
 
-public class LogReaderV2 extends LogReaderAbs implements LogReader{
+public class LogReaderV2 extends LogReaderAbs implements LogReader {
 
     /**
      * @param fileLog name of the log file
      */
-    public LogReaderV2(File fileLog) {
+    public LogReaderV2(final File fileLog) {
         super(fileLog);
         this.version = 2;
     }
 
-    protected PlayerItf createPlayer(org.jsoup.nodes.Document doc) {
+    /**
+     * @param doc jsoup Docment to be parsed .
+     * @return the new object player
+     */
+    protected final PlayerItf createPlayer(final org.jsoup.nodes.Document doc) {
         String name = doc.select("b").text().split(":")[0];
         return new Player(name);
     }
 
-    public Document getDoc(){
+
+    /**
+     * generates a Document compatible with mongodb based on the log.
+     * @return a Document
+     */
+    public final Document getDoc() {
         return this.toDoc().append("version", this.version);
     }
 }
