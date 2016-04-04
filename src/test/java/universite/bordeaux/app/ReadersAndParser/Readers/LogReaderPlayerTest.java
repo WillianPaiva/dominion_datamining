57f443a7ffc74e6e38421272aff00c3257483184
Martha Lopez Farfan <mlfarfan@emi.u-bordeaux1.fr>
2016-04-04 16:21:35 +0200

    add test players document

---
+++ src/test/java/universite/bordeaux/app/ReadersAndParser/Readers/LogReaderPlayerTest.java
@@ -0,0 +1,137 @@
+package universite.bordeaux.app.ReadersAndParser.Readers;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.bson.Document;
+
+import universite.bordeaux.app.Constant.ConstantLog;
+import universite.bordeaux.app.ReadersAndParser.CheckCard;
+import universite.bordeaux.app.ReadersAndParser.LogReaderFactory;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * @author mlfarfan
+ *
+ */
+public class LogReaderPlayerTest extends TestCase {
+	
+	protected LogReader lr1;
+	protected LogReader lr2;
+	protected LogReader lr3;
+	protected Document doclog1;
+	protected Document doclog2;
+	protected Document doclog3;
+	
+	protected void setUp() throws Exception {
+		
+		File log = new File("testFolder/20121210/game-20121210-000005-8ef23689.html"); 
+		lr1 = LogReaderFactory.createrReader(log);
+		
+		File log2 = new File("testFolder/20101110/game-20101110-000556-5f5e69d5.html"); 
+		lr2 = LogReaderFactory.createrReader(log2);
+		
+		File log3 = new File("testFolder/20110110/game-20110110-235856-9bb3d73a.html");  
+		lr3 = LogReaderFactory.createrReader(log3);
+		
+		doclog1 = lr1.getDoc();
+		doclog2 = lr2.getDoc();		
+		doclog3 = lr3.getDoc();		
+		
+	}
+	
+	public void testPlayerNotNull() {
+		
+		assertNotNull(doclog1.get(ConstantLog.PLAYERS));
+		assertNotNull(doclog2.get(ConstantLog.PLAYERS));
+		assertNotNull(doclog3.get(ConstantLog.PLAYERS));
+		
+	}
+	
+	public void testPlayerNumber() {
+		
+		ArrayList<Document> players1 = (ArrayList<Document>) doclog1.get(ConstantLog.PLAYERS);
+		ArrayList<Document> players2 = (ArrayList<Document>) doclog2.get(ConstantLog.PLAYERS);
+		ArrayList<Document> players3 = (ArrayList<Document>) doclog3.get(ConstantLog.PLAYERS);
+		
+		assertEquals(players1.size(), 2);
+		assertEquals(players2.size(), 2);
+		assertEquals(players3.size(), 3);
+				
+	}
+	
+	public void testPlayerNames() {
+		
+		ArrayList<Document> players1 = (ArrayList<Document>) doclog1.get(ConstantLog.PLAYERS);
+		ArrayList<Document> players2 = (ArrayList<Document>) doclog2.get(ConstantLog.PLAYERS);
+		ArrayList<Document> players3 = (ArrayList<Document>) doclog3.get(ConstantLog.PLAYERS);
+		
+		assertEquals(players1.get(0).get(ConstantLog.NAME), "Bazaar Trader");
+		assertEquals(players2.get(1).get(ConstantLog.NAME), "fifi");
+		assertEquals(players3.get(2).get(ConstantLog.NAME), "gravitation");
+	}
+	
+	public void testPlayerPoints() {
+		
+		ArrayList<Document> players1 = (ArrayList<Document>) doclog1.get(ConstantLog.PLAYERS);
+		ArrayList<Document> players2 = (ArrayList<Document>) doclog2.get(ConstantLog.PLAYERS);
+		ArrayList<Document> players3 = (ArrayList<Document>) doclog3.get(ConstantLog.PLAYERS);
+		
+		assertEquals(players1.get(1).get(ConstantLog.POINTS), 39);
+		assertEquals(players2.get(0).get(ConstantLog.POINTS), 36);
+		assertEquals(players3.get(1).get(ConstantLog.POINTS), 25);
+		
+	}
+	
+	public void testPlayerTurns() {
+		
+		ArrayList<Document> players1 = (ArrayList<Document>) doclog1.get(ConstantLog.PLAYERS);
+		ArrayList<Document> players2 = (ArrayList<Document>) doclog2.get(ConstantLog.PLAYERS);
+		ArrayList<Document> players3 = (ArrayList<Document>) doclog3.get(ConstantLog.PLAYERS);
+		
+		assertEquals(players1.get(0).get(ConstantLog.TURNS), 28);
+		assertEquals(players2.get(1).get(ConstantLog.TURNS), 16);
+		assertEquals(players3.get(2).get(ConstantLog.TURNS), 8);
+		
+	}
+	
+	public void testPlayerNumberCardVictory() {
+		
+		ArrayList<Document> players1 = (ArrayList<Document>) doclog1.get(ConstantLog.PLAYERS);
+		ArrayList<Document> players2 = (ArrayList<Document>) doclog2.get(ConstantLog.PLAYERS);
+		ArrayList<Document> players3 = (ArrayList<Document>) doclog3.get(ConstantLog.PLAYERS);
+		
+		Document victoryCards1 = (Document) players1.get(1).get(ConstantLog.VICTOYCARDS);
+		Document victoryCards2 = (Document) players2.get(1).get(ConstantLog.VICTOYCARDS);
+		Document victoryCards3 = (Document) players3.get(0).get(ConstantLog.VICTOYCARDS);
+		
+		assertNotNull(victoryCards1);
+		assertNotNull(victoryCards2);
+		assertNotNull(victoryCards3);
+
+		assertEquals(victoryCards1.size(), 1);
+		assertEquals(victoryCards2.size(), 3);
+		assertEquals(victoryCards3.size(), 3);
+		
+		//probleme Colonies Duchies
+		
+		
+		assertEquals(victoryCards1.get(CheckCard.verifyCard("Province")), 1);
+		assertEquals(victoryCards2.get(CheckCard.verifyCard("Great Hall")), 1);
+		assertEquals(victoryCards3.get(CheckCard.verifyCard("Duchy")), 1);
+		
+	}
+	
+	
+	public static Test suite(){
+		TestSuite suite = new TestSuite(LogReaderPlayerTest.class);
+		return suite;	
+	}
+		
+	public static void main (String[] args){
+		junit.textui.TestRunner.run(suite());
+	}
+	
+}
